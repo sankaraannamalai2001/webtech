dd8008aa3ffdb8cd9b6e6847935e60f366b61709
sankaraannamalai2001 <81792506+sankaraannamalai2001@users.noreply.github.com>
2021-09-13 11:54:22 +0530

    cgpa calculator

---
+++ javascript/cgpa.js
@@ -0,0 +1,19 @@
+const resultBtn = document.querySelector("button");
+
+resultBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getValues();
+});
+function getValues() {
+  let sum = 0,
+    count = 0;
+  const gpa = document.getElementsByName("gpa[]");
+  for (i of gpa) {
+    if (Number(i.value) == 0) continue;
+    sum = sum + Number(i.value);
+    count += 1;
+  }
+
+  const cgpa = sum / count;
+  document.querySelector(".cgpa").textContent = cgpa;
+}
